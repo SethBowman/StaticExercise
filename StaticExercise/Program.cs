729d7d6a4ad2b38b36b902b0802058cf4da61292
Seth Bowman <brandonsethbowman@gmail.com>
2021-04-15 11:44:32 -0500

    inital commit

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What up, G? Welcome to my Temp Converter application.");
+            Console.WriteLine("Gone head and enter a temp in Fahrenheit to convert dat shiz to Celsius, yo.");
+
+            double f = double.Parse(Console.ReadLine());
+            Console.WriteLine($"Word. So the temp in Celsius is : {TempConverter.FahrenheitToCelsius(f)}.");
+
+
+            Console.WriteLine("Aight, dawg. Now gone and enter a temp in Celsius to convert to Fahrenheit and all dat. ");
+
+
+            double c = double.Parse(Console.ReadLine());
+            Console.WriteLine($"Bet it up. The temp in Fahrenheit is : {TempConverter.CelsiusToFahrenheit(c)}.");
+            
+
+            Console.WriteLine($"Nice, my brotha. So we got {TempConverter.FahrenheitToCelsius(f)} for the first and {TempConverter.CelsiusToFahrenheit(c)} for the second.\n Rinse and repeat as necessary, yo.\n I'm out dis mf.");
+
+
+
+        }
+    }
+}
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double f)
+        {
+            return (f - 32) / 1.8;
+        }
+
+
+        public static double CelsiusToFahrenheit(double c)
+        {
+            return (c * 1.8) + 32;
+        }
+
+
+    }
+}
