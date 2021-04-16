7c305b79c7f4d18a80ea13aa94486d3350a4e97a
Seth Bowman <brandonsethbowman@gmail.com>
2021-04-16 09:57:47 -0500

    exercise completed

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -6,21 +6,21 @@ namespace StaticExercise
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("What up, G? Welcome to my Temp Converter application.");
-            Console.WriteLine("Gone head and enter a temp in Fahrenheit to convert dat shiz to Celsius, yo.");
+            Console.WriteLine("Hello user. Welcome to my Temp Converter application.");
+            Console.WriteLine("Please enter a temp in Fahrenheit to convert into Celsius.");
 
             double f = double.Parse(Console.ReadLine());
-            Console.WriteLine($"Word. So the temp in Celsius is : {TempConverter.FahrenheitToCelsius(f)}.");
+            Console.WriteLine($"You temp in Celsius is : {TempConverter.FahrenheitToCelsius(f)}.");
 
 
-            Console.WriteLine("Aight, dawg. Now gone and enter a temp in Celsius to convert to Fahrenheit and all dat. ");
+            Console.WriteLine("Now, please enter a temp in Celsius to convert into Fahrenheit. ");
 
 
             double c = double.Parse(Console.ReadLine());
-            Console.WriteLine($"Bet it up. The temp in Fahrenheit is : {TempConverter.CelsiusToFahrenheit(c)}.");
+            Console.WriteLine($"Your temp in Fahrenheit is : {TempConverter.CelsiusToFahrenheit(c)}.");
             
 
-            Console.WriteLine($"Nice, my brotha. So we got {TempConverter.FahrenheitToCelsius(f)} for the first and {TempConverter.CelsiusToFahrenheit(c)} for the second.\n Rinse and repeat as necessary, yo.\n I'm out dis mf.");
+            Console.WriteLine($"We have your tmeps as {TempConverter.FahrenheitToCelsius(f)} for the first and {TempConverter.CelsiusToFahrenheit(c)} for the second.\n End program.");
 
 
 
